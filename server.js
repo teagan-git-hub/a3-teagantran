bf5083915d9809ab9f86c4de36d4456e34d3e3e2
teagan-git-hub <teagan.m.tran@gmail.com>
2026-09-04 15:06:03 -0400

    Added MongoDB and Express

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
-const express = require( 'express' ),
-      app = express()
+const express    = require('express'),
+      app        = express(),
+      dreams     = []
 
-const logger = (req,res,next) => {
-  console.log( 'url:', req.url )
-  next()
-}
+app.use( express.static( 'public' ) )
+app.use( express.static( 'views'  ) )
+app.use( express.json() )
 
-app.use( logger )
-
-app.get( '/', ( req, res ) => res.send( 'Hello World!' ) )
+app.post( '/submit', express.json(), ( req, res ) {
+  dreams.push( req.body.newdream )
+  res.writeHead( 200, { 'Content-Type': 'application/json'})
+  res.end( JSON.stringify( dreams ) )
+})
 
 app.listen( process.env.PORT || 3000 )
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://teaganmtran_db_user:6lib2TAg7LCXbq0C@a3-teagantran.yufho2h.mongodb.net/?appName=a3-teagantran";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
