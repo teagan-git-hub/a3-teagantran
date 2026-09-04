be7d48912fb57a3b1a2c638221ac1278e6732ba1
teagan-git-hub <teagan.m.tran@gmail.com>
2026-09-04 14:31:34 -0400

    npm install express --save

--- server.js
+++ server.js
@@ -1,45 +1,13 @@
-// server
-const express = require('express'),
-      app = express(),
-      dreams = []
+const express = require( 'express' ),
+      app = express()
 
-const middleware_post = ( req, res, next ) => {
-  let dataString = ''
-
-  req.on( 'data', function( data ) {
-    dataString += data 
-  })
-
-  req.on( 'end', function() {
-    const json = JSON.parse( dataString )
-    dreams.push( json )
-
-    // add a 'json' field to our request object
-    // this field will be available in any additional
-    // routes or middleware.
-    req.json = JSON.stringify( dreams )
-
-    // advance to next middleware or route
-    next()
-  })
+const logger = (req,res,next) => {
+  console.log( 'url:', req.url )
+  next()
 }
 
-app.use( express.static('./') )
-app.post( '/submit', middleware_post )
+app.use( logger )
 
-app.post( '/submit', ( req, res ) => {
-  // our request object now has a 'json' field in it from our previous middleware
-  res.writeHead( 200, { 'Content-Type': 'application/json'})
-  res.end( req.json )
-})
+app.get( '/', ( req, res ) => res.send( 'Hello World!' ) )
 
-const listener = app.listen( process.env.PORT || 3000 )
-
-// run in developer's console
-fetch( '/submit', {
-  method:  'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body:    JSON.stringify(['test'])
-})
-.then( response => response.json() )
-.then( console.log ) 
+app.listen( process.env.PORT || 3000 )
